ff6f167035ef52fdde86628ebde61f5038d980b8
azizywn <63053486+azizywn@users.noreply.github.com>
2020-12-10 16:06:09 +0700

    Add files via upload

---
+++ Table/PropinsiTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PropinsiTableViewCell.swift
+//  Table
+//
+//  Created by Muhammd Rafa Al bani on 11/12/20.
+//
+
+import UIKit
+
+class PropinsiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var propinsiView: UIView!
+    @IBOutlet weak var propinsiImg: UIImageView!
+    @IBOutlet weak var propinsiLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
